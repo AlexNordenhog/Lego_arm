7863bdc5921b132a0a1c58849524f18307cca2cc
AlexNordenhog <alex.nordenhog@gmail.com>
2023-04-11 19:12:10 +0200

    Added a skeleton of functions for sprint 1

--- main.py
+++ main.py
@@ -21,16 +21,42 @@ color_sensor = ColorSensor(Port.S2)
 
 # Write your program here.
 ev3.speaker.beep()
-"exempelkod från testet"
-#arm_motor.run_time(-100, 5, then=Stop.HOLD, wait=True)
-#arm_motor.run_until_stalled(-100, then=Stop.COAST, duty_limit=50)
-#claw_motor.run_until_stalled(-100, then=Stop.COAST, duty_limit=50)
-#claw_motor.run_until_stalled(100, then=Stop.COAST, duty_limit=50)
-#claw_motor.run_time(50, 2000, then=Stop.HOLD, wait=True) Öppnar claw 
-#arm_motor.run_until_stalled(-100, then=Stop.COAST, duty_limit=100)
-#claw_motor.run_time(-50, 2000, then=Stop.HOLD, wait=True)
-#arm_motor.run_until_stalled(-100, then=Stop.COAST, duty_limit=100)
-#claw_motor.run_time(50, 3000, then=Stop.HOLD, wait=True)
-#arm_motor.run_until_stalled(70, then=Stop.COAST, duty_limit=50)
-#claw_motor.run_time(-50, 2000, then=Stop.HOLD, wait=True)
-#test
+arm_motor.control.limits(speed=100, acceleration=100)
+claw_motor.control.limits(speed=100, acceleration=100)
+
+def basic_pickup():
+    turning_motor.run_target(speed=100, target_angle=0)
+    #Down
+    arm_motor.run_target(speed=100, target_angle=-90)#Determine correct target
+    #Bite
+    claw_motor.run_target(speed=100, target_angle=90) 
+    #Up
+    arm_motor.run_target(speed=100, target_angle=0)#Determine correct target
+
+def color_analyzer():
+    #awd
+
+def basic_drop():
+    #asd
+
+def detection():
+    #fsadf
+
+def main_menu():
+    while True:
+        print("Please pick a Task for mr.robot /n asdas")
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            Basic_pickup()
+        elif choice == '2':
+            # func 2
+            pass
+        elif choice == '3':
+            # func 3
+            pass
+        elif choice == '4':
+            break
+        else:
+            ev3.speaker.say("Invalid choice")
+
+main_menu()
