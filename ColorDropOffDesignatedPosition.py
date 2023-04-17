f633751be41b27b760fadbab494223cb17bae4e0
AlexNordenhog <alex.nordenhog@gmail.com>
2023-04-17 09:58:30 +0200

    update

--- ColorDropOffDesignatedPosition.py
+++ ColorDropOffDesignatedPosition.py
@@ -29,23 +29,22 @@ color = color_sensor.color()
 # Speak the detected color
 if color == Color.BLACK:
     ev3.speaker.say("The object is black")
- elif color == Color.BLUE:
+elif color == Color.BLUE:
     ev3.speaker.say("The object is blue")
     motor_turn.run_target(speed=100, target_angle=-300, then=Stop.HOLD, wait=True)
     arm_motor.run_until_stalled(200, then=Stop.HOLD, duty_limit=500)
-
- elif color == Color.GREEN:
+elif color == Color.GREEN:
      ev3.speaker.say("The object is green")
- elif color == Color.YELLOW:
+elif color == Color.YELLOW:
     ev3.speaker.say("The object is yellow")
     motor_turn.run_target(speed=100, target_angle=-450, then=Stop.HOLD, wait=True)
     arm_motor.run_until_stalled(200, then=Stop.HOLD, duty_limit=500)
- elif color == Color.RED:
+elif color == Color.RED:
     ev3.speaker.say("The object is red")
     motor_turn.run_target(speed=100, target_angle=-600, then=Stop.HOLD, wait=True)
     arm_motor.run_until_stalled(200, then=Stop.HOLD, duty_limit=500)
 
- elif color == Color.WHITE:
+elif color == Color.WHITE:
     ev3.speaker.say("The object is white")
- else:
+else:
     ev3.speaker.say("The object color is unknown")
