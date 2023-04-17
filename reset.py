64d6d2722fbfbd5e5018a62b911770867eb877af
AlexNordenhog <alex.nordenhog@gmail.com>
2023-04-17 11:02:27 +0200

    update

--- reset.py
+++ reset.py
@@ -27,9 +27,9 @@ ev3.speaker.beep()
 
 arm_motor.run_until_stalled(200, then=Stop.HOLD , duty_limit=50)
 claw_motor.run_until_stalled(200, then=Stop.HOLD , duty_limit=50)
-motor_turn.run_until_stalled(100, then=Stop.HOLD , duty_limit=10)
+motor_turn.run_until_stalled(300, then=Stop.HOLD , duty_limit=15)
 
-if touch_sensor.pressed() == True:
-    motor_turn.HOLD()
+# if touch_sensor.pressed() == True:
+#     motor_turn.HOLD()
 
 claw_motor.run_target(speed=100, target_angle=-70, then=Stop.HOLD, wait=True)
