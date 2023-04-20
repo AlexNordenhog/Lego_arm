0886b715df5b98bf88f467e60693aa96bb81ec98
AlexNordenhog <alex.nordenhog@gmail.com>
2023-04-20 13:02:36 +0200

    okj

--- Color.py
+++ Color.py
@@ -46,35 +46,34 @@ def color_checker():
         ev3.speaker.say("The object color is unknown")  
 
 
-# Tar upp föremålet och håller det framför sensorn
+# # Tar upp föremålet och håller det framför sensorn
 
-# arm_motor.run_target(speed=100, target_angle=-300, then=Stop.HOLD, wait=True)
+# # arm_motor.run_target(speed=100, target_angle=-300, then=Stop.HOLD, wait=True)
 
-# motor_turn.run_target(speed=100, target_angle=-300, then=Stop.HOLD, wait=True)
+# # motor_turn.run_target(speed=100, target_angle=-300, then=Stop.HOLD, wait=True)
 
-# claw_motor.run_target(speed=100, target_angle=-70, then=Stop.HOLD, wait=True)  
+# # claw_motor.run_target(speed=100, target_angle=-70, then=Stop.HOLD, wait=True)  
 
-# arm_motor.run_until_stalled(200, then=Stop.HOLD , duty_limit=500)
-claw_motor.run_until_stalled(200, then=Stop.HOLD , duty_limit=500)
-# Håller brickan framför    
-arm_motor.run_target(speed=100, target_angle=-197, then=Stop.HOLD, wait=True)
+# # arm_motor.run_until_stalled(200, then=Stop.HOLD , duty_limit=500)
+# claw_motor.run_until_stalled(200, then=Stop.HOLD , duty_limit=500)
+# # Håller brickan framför    
+# arm_motor.run_target(speed=100, target_angle=-197, then=Stop.HOLD, wait=True)
 
-wait(2000)
-# Color
-color = color_sensor.color()
-    # Speak the detected color
-if color == Color.BLACK:
-    ev3.speaker.say("The object is black")
-elif color == Color.BLUE:
-    ev3.speaker.say("The object is blue")
-elif color == Color.GREEN:
-    ev3.speaker.say("The object is green")
-elif color == Color.YELLOW:
-    ev3.speaker.say("The object is yellow")
-elif color == Color.RED:
-    ev3.speaker.say("The object is red")
-elif color == Color.WHITE:
-    ev3.speaker.say("The object is white")
-else:
-    ev3.speaker.say("The object color is unknown")  
-
+# wait(2000)
+# # Color
+# color = color_sensor.color()
+#     # Speak the detected color
+# if color == Color.BLACK:
+#     ev3.speaker.say("The object is black")
+# elif color == Color.BLUE:
+#     ev3.speaker.say("The object is blue")
+# elif color == Color.GREEN:
+#     ev3.speaker.say("The object is green")
+# elif color == Color.YELLOW:
+#     ev3.speaker.say("The object is yellow")
+# elif color == Color.RED:
+#     ev3.speaker.say("The object is red")
+# elif color == Color.WHITE:
+#     ev3.speaker.say("The object is white")
+# else:
+#     ev3.speaker.say("The object color is unknown")  
